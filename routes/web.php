843a1f33a8dd9eeafe9646febab607596a01679b
francis556 <vikrammehra244@gmail.com>
2022-01-30 21:26:40 +0200

    first route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// custom route
+
+Route::get('/', function (){
+    return 'Hello, world!';
 });
+
